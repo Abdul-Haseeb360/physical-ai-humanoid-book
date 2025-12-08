819c34060b0c02164d7f5165461163fe5918ae59
Abdul-Haseeb360 <aheducation789@gmail.com>
2025-12-08 17:32:46 +0500

    update:config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://abdul-haseeb360.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/physical-ai-humanoid-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'abdul-haseeb360', // Usually your GitHub org/user name.
+  projectName: 'physical-ai-humanoid-book', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
